b4bbeffb5be0e9f3501a8469e9cb7fb24146be6b
Barsil Nzola <barsiljohn@gmail.com>
2020-06-15 07:55:49 +0300

    added a form component for new quotes input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { HighlightDirective } from './highlight.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { DateRangePipe } from './date-range.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DateRangePipe } from './date-range.pipe';
     QuoteDetailComponent,
     HighlightDirective,
     DateCountPipe,
-    DateRangePipe
+    DateRangePipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
