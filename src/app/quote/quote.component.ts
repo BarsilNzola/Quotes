ee811bc2ceda213d6bdf89d65da857fa26658dc0
Barsil Nzola <barsiljohn@gmail.com>
2020-06-15 03:48:47 +0300

    added a date counter pipe

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1,'Named must be your fear before banish it you can.','Master Yoda','John Nzola',0,0),
-    new Quote(2,'We shall destroy the ones called the turtles who call themselves the turtles.','Krang','John Nzola',0,0),
+    new Quote(1,'Named must be your fear before banish it you can.','Master Yoda','John Nzola',0,0,new Date(2020,6,15)),
+    new Quote(2,'We shall destroy the ones called the turtles who call themselves the turtles.','Krang','John Nzola',0,0,new Date(2020,6,15)),
   ];
+  preNum:number
+  postNum:number
+  count:number
 
   toggleDetails(index){
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
@@ -38,6 +41,18 @@ export class QuoteComponent implements OnInit {
     }
   }
 
+  highestUpvote(){
+    this.preNum=0
+    this.postNum=0
+
+    for(this.count=0 ; this.count < this.quotes.length; this.count++) {
+      this.postNum = this.quotes[this.count].upvote;
+      if(this.postNum > this.preNum){this.preNum = this.postNum}
+    }
+    return  this.preNum
+  }
+
+  
   constructor(){}
 
   ngOnInit(){
